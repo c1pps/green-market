1c0b525340c2ccf1f42f7ffd0b087f433c0d09bc
Your Name <jneveu.work@gmail.com>
2025-12-11 20:18:36 +0100

    home page finished with responsive and all

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -2,11 +2,11 @@ import Logo from '../../../public/img/logo.svg'
 
 export default function Footer() {
     return (
-        <div className='flex justify-between items-start'>
-            <div>
-                <p className="AGN3D-Font underline text-[32px] mb-[10px]">Page</p>
+        <div className='flex flex-col md:flex-row justify-between items-start'>
+            <div className='mb-[40px] md:mb-0'>
+                <p className="AGN3D-Font underline text-[28px] md:text-[32px] mb-[10px]">Page</p>
                 <nav>
-                    <ul className="flex flex-col VSR-Font text-[34px] uppercase">
+                    <ul className="flex flex-col VSR-Font text-[24px] md:text-[34px] uppercase">
                         <li>
                             <a href="" className="group text-black transition-all duration-300 ease-in-out"><span className='bg-left-bottom bg-gradient-to-r from-black to-black bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-300 ease-in-out'>Accueil</span></a>
                         </li>
@@ -22,10 +22,10 @@ export default function Footer() {
                     </ul>
                 </nav>
             </div>
-            <div>
-                <p className="AGN3D-Font underline text-[32px] mb-[10px]">Support</p>
+            <div className='mb-[40px] md:mb-0'>
+                <p className="AGN3D-Font underline text-[28px] md:text-[32px] mb-[10px]">Support</p>
                 <nav>
-                    <ul className="flex flex-col VSR-Font text-[34px] uppercase">
+                    <ul className="flex flex-col VSR-Font text-[24px] md:text-[34px] uppercase">
                         <li>
                             <a href="" className="group text-black transition-all duration-300 ease-in-out"><span className='bg-left-bottom bg-gradient-to-r from-black to-black bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-300 ease-in-out'>faq</span></a>
                         </li>
@@ -36,7 +36,7 @@ export default function Footer() {
                 </nav>
             </div>
             <div>
-                <img src={Logo} alt="Logo" className='h-[200px]' />
+                <img src={Logo} alt="Logo" className='h-[120px] md:h-[200px]' />
             </div>
         </div>
     )
--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
                 <Nav></Nav>
             </header>
             <div className='my-[45px]'>
-                <div className='mx-[50px] md:mx-[400px]'>
+                <div className='mx-[20px] md:mx-[400px]'>
                     <div className='flex justify-between'>
                         <img src={Logo} alt="Logo" className='w-[15%]' />
                         <img src={illustrationBanner} alt="Illustration en guise de bannière" className='w-[80%]' />
@@ -24,25 +24,25 @@ export default function Home() {
                     <div className='mb-[120px]'>
                         {/* <h1 className='uppercase leading-none whitespace-nowrap overflow-hidden text-[clamp(64px,12vw,140px)] tracking-[clamp(16px,4vw,66px)]'>Greenmarket</h1> */}
                         <img src={LogoText} alt="Logo texte" className='w-[100%]' />
-                        <p className='AGN3D-Font text-[20px] md:text-[36px] mb-[30px] mt-[10px] leading-5.5 md:leading-6'>Des produits responsables pour un quotidien plus vert — faites le choix durable.</p>
+                        <p className='AGN3D-Font text-[20px] md:text-[36px] mb-[30px] md:mb-[50px] mt-[10px] md:mt-[20px] leading-5.5 md:leading-6'>Des produits responsables pour un quotidien plus vert — faites le choix durable.</p>
                         <Link to="/produits" className='AGN3D-Font text-[20px] md:text-[36px] border border-2 md:border-3 border-black bg-transparent px-[10px] md:px-[20px] py-[8px] md:py-[10px] hover:bg-black hover:text-white transition duration-300 ease-in-out'>Découvrir les produits</Link>
                     </div>
-                    <div className='mb-[120px]'>
-                        <div className='flex justify-between mb-[20px] uppercase'>
-                            <h2 className='text-[32px] md:text-[64px]'>_ notre séléction</h2>
-                            {/* <Link to="/produits" className="AGN3D-Font text-[64px] group text-black transition-all duration-300 ease-in-out"><span className="bg-left-bottom bg-gradient-to-r from-black to-black bg-[length:0%_4px] bg-no-repeat group-hover:bg-[length:100%_4px] transition-all duration-300 ease-in-out">_ Les autres</span></Link> */}
-                        </div>
-                        <ALaUne></ALaUne>
+                </div>
+                <div className='mb-[120px] ms-[20px] md:mx-[400px]'>
+                    <div className='flex justify-between mb-[20px] uppercase'>
+                        <h2 className='text-[32px] md:text-[64px]'>_ notre séléction</h2>
+                        {/* <Link to="/produits" className="AGN3D-Font text-[64px] group text-black transition-all duration-300 ease-in-out"><span className="bg-left-bottom bg-gradient-to-r from-black to-black bg-[length:0%_4px] bg-no-repeat group-hover:bg-[length:100%_4px] transition-all duration-300 ease-in-out">_ Les autres</span></Link> */}
                     </div>
+                    <ALaUne></ALaUne>
                 </div>
                 <div>
-                    <div className='uppercase bg-[#FFC248] px-[50px] md:px-[400px] py-[40px]'>
+                    <div className='uppercase bg-[#FFC248] ps-[20px] md:px-[400px] py-[40px]'>
                         <h2 className='text-[32px] md:text-[64px] mb-[20px]'>_ ils en pensent quoi ?</h2>
                         <Avis></Avis>
                     </div>
                 </div>
             </div>
-            <footer className='mx-[400px] my-[80px]'>
+            <footer className='mx-[20px] md:mx-[400px] my-[40px] md:my-[80px]'>
                 <Footer></Footer>
             </footer>
         </div>
